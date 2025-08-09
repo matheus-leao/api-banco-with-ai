2304b06d4cf5594659438768b5eacecb70f3a7a3
Matheus Leao <matsouzaleao@gmail.com>
2025-08-09 16:07:40 -0300

    Create transferController test file

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,34 @@
+// Libs
+const request = require('supertest');
+const sinon = require('sinon')
+const { expect } = require('chai');
+
+// App
+const app = require('../../app');
+
+// Testes
+describe('Transfer Controller', ()=>{
+    describe('GET /transfers', ()=>{
+        it('Buscar todas as transferências existentes', async ()=>{
+            const response = await request(app).get('/transfers');
+            expect(response.statusCode).to.equal(200)
+        })
+    })
+
+    describe('POST /transfers', ()=>{
+        it('Quando informo remetente e destinatários inexistentes recebo 400', async ()=> {
+            const response = await request(app)
+                .post('/transfer')
+                .send({                  
+                    from: "Julio",
+                    to: "Matheus",
+                    amount: 10000
+            });
+
+            expect(response.status).to.equal(400)
+            expect(response.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+        })
+
+    })
+    
+})
